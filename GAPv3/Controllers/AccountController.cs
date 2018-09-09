96c4ae2d102cf7b4cac5936df86edcaafb62ba92
Josippark <josippavlovic7@gmail.com>
2018-09-09 13:08:30 +0200

    login functionality

---
+++ GAPv3/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using GAPv3.DAL;
+using GAPv3.Models;
+
+namespace GAPv3.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User UserInput, string ReturnUrl = "")
+        {
+            //using (GAPv2Context dc = new GAPv2Context())
+            //{
+            //    var user = dc.User.Where(a => a.Email.Equals(UserInput.Email) && 
+            //                                  a.Password.Equals(UserInput.Password)).FirstOrDefault();
+            //    if (user != null)
+            //    {
+            //        FormsAuthentication.SetAuthCookie(user.Email, true);
+            //        if (Url.IsLocalUrl(ReturnUrl))
+            //        {
+            //            return Redirect(ReturnUrl);
+            //        }
+            //        else
+            //        {
+            //            return RedirectToAction("Index", "Home");
+            //        }
+            //    }
+            //}
+            //ModelState.Remove("Password");
+            //return View();
+            //if (ModelState.IsValid)
+            //{
+            //    var isValidUser = Membership.ValidateUser(UserInput.Email, UserInput.Password);
+            //    if (isValidUser)
+            //    {
+            //        FormsAuthentication.SetAuthCookie(UserInput.Email,true);
+            //        if (Url.IsLocalUrl(ReturnUrl))
+            //        {
+            //            return Redirect(ReturnUrl);
+            //        }
+            //        else
+            //        {
+            //            return RedirectToAction("Index", "Home");
+            //        }
+            //    }
+            //}
+
+
+            if (ModelState.IsValid)
+            {
+                bool isValidUser = Membership.ValidateUser(UserInput.Email, UserInput.Password);
+                if (isValidUser)
+                {
+                    User user = null;
+                    using (GAPv3Context dc = new GAPv3Context())
+                    {
+                        user = dc.Users.Where(a => a.Email.Equals(UserInput.Email)).FirstOrDefault();
+                    }
+
+                    if (user != null)
+                    {
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        string data = js.Serialize(user);
+                        FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, user.Email, DateTime.Now,
+                            DateTime.Now.AddMinutes(30), true, data);
+                        string encToken = FormsAuthentication.Encrypt(ticket);
+                        HttpCookie authoCookies = new HttpCookie(FormsAuthentication.FormsCookieName, encToken);
+                        Response.Cookies.Add(authoCookies);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+
+            ModelState.Remove("Password");
+            return View();
+
+        }
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+    }
+}
---
+++ GAPv3/CustomRoleProvider.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Security;
+using GAPv3.DAL;
+
+namespace GAPv3
+{
+    public class CustomRoleProvider : RoleProvider
+    {
+        private int _cacheTimeoutInMinute = 20;
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string email)
+        {
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                return null;
+            }
+
+            //check cache
+            var cacheKey = string.Format("{0}_role", email);
+            if (HttpRuntime.Cache[cacheKey] != null)
+            {
+                return (string[])HttpRuntime.Cache[cacheKey];
+            }
+            string[] roles = new string[] { };
+            using (GAPv3Context dc = new GAPv3Context())
+            {
+                roles = (from a in dc.Roles
+                         join b in dc.UserRoles on a.RoleId equals b.RoleId
+                         join c in dc.Users on b.UserId equals c.UserId
+                         where c.Email.Equals(email)
+                         select a.RoleName).ToArray<string>();
+                if (roles.Count() > 0)
+                {
+                    HttpRuntime.Cache.Insert(cacheKey, roles, null, DateTime.Now.AddMinutes(_cacheTimeoutInMinute), Cache.NoSlidingExpiration);
+
+                }
+            }
+            return roles;
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
